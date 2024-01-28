ca5af091a9c5f9ad36e6f4ceb9cc57f0ef16cbeb
Amy-j-seo <amywonderland77@gmail.com>
2024-01-28 16:50:27 +0900

    [instagram clone] #7 photo upload function

--- flutter_clone_instagram/lib/src/pages/upload.dart
+++ flutter_clone_instagram/lib/src/pages/upload.dart
@@ -1,6 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_instagram/src/components/image_data.dart';
 import 'package:get/get.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class Upload extends StatefulWidget {
   const Upload({super.key});
@@ -10,11 +13,67 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  var albums = <AssetPathEntity>[];
+  var headerTitle = '';
+  var imageList = <AssetEntity>[];
+  AssetEntity? selectedImage;
+  @override
+  void initState() {
+    super.initState();
+    _loadPhotos();
+  }
+
+  void _loadPhotos() async {
+    var result = await PhotoManager.requestPermissionExtend();
+    if (result.isAuth) {
+      //
+      albums = await PhotoManager.getAssetPathList(
+        type: RequestType.image,
+        filterOption: FilterOptionGroup(
+          imageOption: const FilterOption(
+            sizeConstraint: SizeConstraint(minHeight: 100, minWidth: 100),
+          ),
+          orders: [
+            const OrderOption(type: OrderOptionType.createDate, asc: false),
+          ],
+        ),
+      );
+      _loadData();
+    } else {
+      //do something
+      print('auth not done');
+    }
+  }
+
+  void _loadData() async {
+    headerTitle = albums.first.name;
+    await _pagingPhotos();
+    update();
+  }
+
+  Future<void> _pagingPhotos() async {
+    var photos = await albums.first.getAssetListPaged(page: 0, size: 30);
+    print(photos.length);
+    imageList.addAll(photos);
+    selectedImage = imageList.first;
+  }
+
+  void update() => setState(() {});
+
   Widget _imagePreview() {
+    var width = MediaQuery.of(context).size.width;
     return Container(
-      width: Get.width,
-      height: Get.width,
+      width: width,
+      height: width,
       color: Colors.orangeAccent,
+      child: selectedImage == null
+          ? Container()
+          : _photoWidget(selectedImage!, width.toInt(), builder: (data) {
+              return Image.memory(
+                data,
+                fit: BoxFit.cover,
+              );
+            }),
     );
   }
 
@@ -24,19 +83,73 @@ class _UploadState extends State<Upload> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Text(
-                  'Gallery',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
                   ),
                 ),
-                Icon(Icons.arrow_drop_down),
-              ],
+                isScrollControlled: true,
+                constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top),
+                builder: (_) => Container(
+                  height: albums.length*60,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children:
+                   [
+                    Center(
+                      child: Container(
+                     margin: const EdgeInsets.only(top: 7),
+                       decoration: BoxDecoration(
+                         borderRadius: BorderRadius.circular(10),
+                         color: Colors.black54,
+                       ),
+                     width: 40,
+                     height: 4,
+                   ),
+                    ),
+                     Expanded(
+                       child: SingleChildScrollView(
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.stretch,
+                           children: List.generate(
+                             albums.length,
+                             // 100,
+                                 (index) => Container(
+                               padding: const EdgeInsets.symmetric(
+                                 vertical: 15, horizontal: 15,
+                               ),
+                               child: Text(albums[index].name),
+                             ),
+                           ),
+                         ),
+                       ),
+                     ),
+
+
+                   ]
+                  ),
+                ),
+              );
+            },
+            child: Padding(
+              padding:const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    headerTitle,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const Icon(Icons.arrow_drop_down),
+                ],
+              ),
             ),
           ),
           Row(
@@ -89,13 +202,35 @@ class _UploadState extends State<Upload> {
         mainAxisSpacing: 1.5,
         crossAxisSpacing: 1.5,
       ),
-      itemCount: 100,
+      itemCount: imageList.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          color: Colors.red,
-        );
+        return _photoWidget(imageList[index], 200, builder: (data) {
+          return GestureDetector(
+            onTap: () {
+              selectedImage = imageList[index];
+              update();
+            },
+            child: Opacity(
+              opacity: imageList[index] == selectedImage ? 0.3 : 1,
+              child: Image.memory(data, fit: BoxFit.cover),
+            ),
+          );
+        });
       },
     );
+  }
+
+  Widget _photoWidget(AssetEntity asset, int size,
+      {required Widget Function(Uint8List) builder}) {
+    return FutureBuilder(
+        future: asset.thumbnailDataWithSize(ThumbnailSize(size, size)),
+        builder: (_, AsyncSnapshot<Uint8List?> snapshot) {
+          if (snapshot.hasData) {
+            return builder(snapshot.data!);
+          } else {
+            return Container();
+          }
+        });
   }
 
   @override
@@ -106,7 +241,9 @@ class _UploadState extends State<Upload> {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: GestureDetector(
-          onTap: () {},
+          onTap: () {
+            print('clicked?');
+          },
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: ImageData(IconPath.closeImage),
